f3276bd6ee316993f47594f0d1d562809f2f892b
johnpaschoud <john@paschoud.org>
2023-06-27 16:53:02 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+// Uses Adafruit Neopixel driver extension. Connect Din of strip to ZiP pin of Kitronik Create motor board (is u:bit pin 1).
+let strip = neopixel.create(DigitalPin.P0, 32, NeoPixelMode.RGB)
+basic.forever(function () {
+	
+})
